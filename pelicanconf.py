c093f40bb1caca656c31d1437c401a85e424b0a8
Jinserk Baik <jinserk.baik@gmail.com>
2016-09-07 05:40:31 -0500

    comment out static path

--- pelicanconf.py
+++ pelicanconf.py
@@ -37,7 +37,7 @@ PLUGIN_PATHS = ['../pelican-plugins']
 PLUGINS = ['gravatar', 'render_math']
 
 # theme
-STATIC_PATHS = ['images', 'pdfs']
+#STATIC_PATHS = ['images', 'pdfs']
 THEME = "../pelican-themes/pure"
 
 COVER_IMG_URL = "images/main.jpg"

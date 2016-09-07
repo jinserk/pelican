1850e93f3ff7c9a40f82857d8a6c11e38730feea
Jinserk Baik <jinserk.baik@gmail.com>
2016-09-07 05:58:19 -0500

    update publish.sh

--- publish.sh
+++ publish.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+echo "make html && make publish"
 make html || exit 1;
 make publish || exit 1;
+
+echo -e "git commit"
 git commit -a || exit 1;
+
+echo -e "git push"
 git push || exit 1;
 git subtree push --prefix=output jinserk master || exit 1;
